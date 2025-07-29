26fe962fd3f590d8dd7bfd729c49f38a8ced3919
CassioLima <lima164@gmail.com>
2025-07-29 14:32:38 -0300

    subindo a avaliação

---
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -0,0 +1,141 @@
+﻿using Ambev.DeveloperEvaluation.Application;
+using Ambev.DeveloperEvaluation.Common.HealthChecks;
+using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.IoC;
+using Ambev.DeveloperEvaluation.ORM;
+using Ambev.DeveloperEvaluation.WebApi.Middleware;
+using MassTransit;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Ambev.DeveloperEvaluation.Common.Logging;
+using MongoDB.Driver;
+using StackExchange.Redis;
+using Microsoft.Extensions.Caching.Distributed;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.AddDefaultLogging();
+
+            Log.Information("Starting web application");
+
+            builder.Services.AddControllers();
+
+            // FluentValidation
+            builder.Services.AddFluentValidationAutoValidation();
+            builder.Services.AddFluentValidationClientsideAdapters();
+            builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+            //HealthChecks
+            builder.Services.AddEndpointsApiExplorer();
+            builder.AddBasicHealthChecks();
+            builder.Services.AddSwaggerGen();
+
+            //Conf do Postgree
+            builder.Services.AddDbContext<DefaultContext>(options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
+                ).LogTo(Console.WriteLine, LogLevel.Information)
+            );
+
+            builder.Services.AddJwtAuthentication(builder.Configuration);
+            builder.RegisterDependencies();
+
+            //AutoMapper
+            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
+
+            //MediatR
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(
+                    typeof(ApplicationLayer).Assembly,
+                    typeof(Program).Assembly
+                );
+            });
+
+            //MassTransit
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            builder.Services.AddMassTransit(x =>
+            {
+                x.AddConsumer<SaleCreatedConsumer>();
+                x.AddConsumer<SaleModifiedConsumer>();
+                x.AddConsumer<SaleCancelledConsumer>();
+                x.AddConsumer<ItemCancelledConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+
+            
+            //Redis
+            builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis")??"");
+                configuration.AbortOnConnectFail = false;
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetConnectionString("Redis");
+                options.InstanceName = "MyApp:";
+            });
+
+
+            var app = builder.Build();
+
+
+            // Middleware de validação customizada
+            app.UseMiddleware<ValidationExceptionMiddleware>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                app.MapGet("/cache-test", async (IDistributedCache cache) =>
+                {
+                    await cache.SetStringAsync("key1", "🔥 Redis funcionando!",
+                        new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+
+                    var valor = await cache.GetStringAsync("key1");
+                    return valor ?? "Valor não encontrado no Redis";
+                });
+
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseBasicHealthChecks();
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
